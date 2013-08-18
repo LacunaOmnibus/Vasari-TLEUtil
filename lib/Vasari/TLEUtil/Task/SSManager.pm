397123efe04160d9b53fb359b8a57167a887a7f0
Vasari <nmccallum7+services@gmail.com>
2013-08-18 20:46:11 +1000

    Almost got something that works here. Added some extra guidelines that this scriot should be working along.

--- lib/Vasari/TLEUtil/Task/SSManager.pm
+++ lib/Vasari/TLEUtil/Task/SSManager.pm
@@ -61,17 +61,49 @@ sub work {
     my $self           = shift;
     my $space_stations = shift; ## Calling it $sss doesn't make much sense without the '
 
-    foreach my $ss (@$space_stations) {
+    my $glc  = $self->bb->resolve(service => 'glc');
+    my $core = $self->bb->resolve(service => 'core');
 
+    ## Array of _all_ upgrades that would need to happen to max out
+    ## $upgrade_modules on each Space Station to $wanted_level.
+    my $upgrades = [];
+
+    foreach my $ss (@$space_stations) {
         ## So now we are left with the task of working out what modules need
         ## upgrading.
-        foreach my $module (@{$ss->{buildings}}) {
 
-            if ($module->{name} ~~ @$upgrade_modules) {
+        ## Get the plans on the station so we can predict (before making the 
+        ## rpc request) weather the upgrade will happen or not.
+        my $pcc      = $core->extract_building($ss->{buildings}, 'Station Command Center');
+        my $pcc_obj  = $glc->building(id => $pcc->{id}, type => 'StationCommand');
+        $ss->{plans} = $pcc_obj->view_plans->{plans};
+        
+        foreach my $id (keys %{$ss->{buildings}}) {
+            my $module = $ss->{buildings}->{$id};
 
-                ## Do stuff
+            if ($module->{name} ~~ @$upgrade_modules and
+                $module->{level} < $wanted_level) {
 
-
+                ## Now check if we can upgrade it now or need to take further
+                ## action to get the plan into existence and then upgrade.
+                ## This is where this process gets very interesting...
+                if (grep {$_->{level} == $module->{level} + 1 and $_->{name} eq $module->{name}} @{$ss->{plans}}) {
+                    ## Do the upgrade.
+                    say Dumper $module;
+                }
+                else {
+                    ## This is what needs to happen:
+                    ## - Check if the plan has been made on a planet somewhere.
+                    ##     - If so, move it for the next upgrade run.
+                    ##     - If not, 
+                    ##         - Find a viable planet to make it on, make, send to db.
+                    ##         - Next run we find the plan and move it.
+                    ##         - Then finally, the above code hits the upgrade button.
+                    ## - If all else fails, log in the db that we attempted to make the plan,
+                    ##   but failed for various reasons.
+                    ##     - Need a script to check this part of the db and attempt to remake
+                    ##       the plan at a later date.
+                }
             }
         }
     }
@@ -81,15 +113,14 @@ sub rate_ss {
     my $self      = shift;
     my $buildings = shift;
     
-    my $core = $self->bb->resolve(service => 'core');
-
+    my $core  = $self->bb->resolve(service => 'core');
     my $score = 0;
 
-    foreach my $module (@$modules) {
-        my $building = $core->extract_building($buildings, $module);
+    foreach my $module_name (@$upgrade_modules) {
+        my $module = $core->extract_building($buildings, $module_name);
         
-        if ($building) {
-            $score += $building->{level};
+        if ($module) {
+            $score += $module->{level};
         }
     }
 
